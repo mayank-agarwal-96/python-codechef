a392b47f23a31f10b799afa3c968da452bedefcf
Mayank Agrawal <agarwalm214@gmail.com>
2016-06-25 19:15:28 +0530

    first commit

---
+++ python_codechef/codechef.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Codechef:
+
+
+	@staticmethod	
+	def get_beginner_problems():
+		url = "https://www.codechef.com/problems/school"
+		page = requests.get(url)
+		plain_text = page.text
+		soup = BeautifulSoup(plain_text)
+
+		table = soup.findAll('table',{'class' : 'dataTable'})[0]
+		rows = table.findAll('tr',{'class' : 'problemrow'})
+		
+		problems = []
+		for row in rows:
+			data = row.findAll('a')
+
+			problem_data = {
+				'name' : str(data[0].text).strip('\n'),
+				'url' : str(url+data[0].get('href')),
+				'code' : str(data[1].text),
+				'submit_url' : str(url+data[1].get('href')),
+				'status' : str(data[2].text)
+			}
+			problems.append(problem_data)
+
+
+		return problems
